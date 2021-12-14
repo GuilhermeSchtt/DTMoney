f5f9e5c1238dd2ff89afd661bb54030c493242b0
Guilherme <guilhermehscolpani@gmail.com>
2021-12-13 22:56:51 -0300

    Init Project with CRUD Transactions

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer, Model } from "miragejs";
+
+createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Desenvolvimento DTmoney',
+          amount: 11250,
+          type: 'deposit',
+          category: 'Licenciamento de Software',
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: 'Fast Food Park',
+          amount: 250,
+          type: 'withdraw',
+          category: 'Alimentação',
+          createdAt: new Date(),
+        },
+        {
+          id: 3,
+          title: 'Monitoramento Infraestrutura ',
+          amount: 1000,
+          type: 'deposit',
+          category: 'Monitoramento de Infra/Software',
+          createdAt: new Date(),
+        },
+      ],
+    })
+  },
+
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+    })
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
